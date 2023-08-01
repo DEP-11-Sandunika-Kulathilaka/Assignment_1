41f8bbcb81697d388232a75c1893151056d1942b
Sadunikaku <kulathilakasandunika@gmail.com>
2023-08-01 22:28:52 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,6 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        System.out.printf("\033[31m%1$d\033[30m%5$s\033[32m%2$s\033[30m%5$s\033[33m%3$s\033[30m%5$s\033[34m%4$s \n", 978,3,16,1484100, "-");
+    }
+    
+}
