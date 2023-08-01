6c713878cc2d042477ccd4ac84b608a244121dbd
Sadunikaku <kulathilakasandunika@gmail.com>
2023-08-01 22:30:58 +0530

    Complete Task 6-A

--- Demo6_task_a.java
+++ Demo6_task_a.java
@@ -1,8 +1,7 @@
-public class task-a {
+public class Demo6_task_a {
     public static void main(String[] args) {
         System.out.printf("\033[41m[\033[30m%1$7d\033[42m\033[30m%2$-7s]\033[49m \n", 5,"0%"); // Task a
 
-        System.out.println();
     }
     
 }
