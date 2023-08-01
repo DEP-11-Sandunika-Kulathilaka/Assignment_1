243742578e563ad638ef19f2ec7a7a54e0361c7e
Sadunikaku <kulathilakasandunika@gmail.com>
2023-08-01 22:27:52 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,9 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int myInt1 = 123;
+        int myInt2 = 456;
+        int myInt3 = 7890;
+        System.out.printf("\033[32m(%1$s)\033[30m %2$s-%3$s \n", myInt1, myInt2, myInt3);
+        //final String Color = "\033[32;1m";
+    }
+}
